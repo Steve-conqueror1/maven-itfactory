1006897259d95331b97681607ec8e9d606d0d564
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2018-11-01 23:53:40 +0300

    use StringUtils and added dependancy to lang3

--- src/main/java/com/maven/itfactory/Application.java
+++ src/main/java/com/maven/itfactory/Application.java
@@ -1,7 +1,16 @@
 package com.maven.itfactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
-    
+
+    public int countWords(String words){
+      String[] separateWords = StringUtils.split(words, ' ');
+      return (separateWords == null)? 0: separateWords.length;
+    }
+
     public Application() {
         System.out.println ("Inside Application itfactory");
     }
@@ -10,5 +19,8 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application itfactory");
 	     Application app = new Application();
+
+       int count = app.countWords("i have four words");
+       System.out.println("Word Count : "+ count);
     }
 }
